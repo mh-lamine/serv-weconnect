ec4ad04d28adbe4283978e1c6cdc17859e396e5c
mh-lamine <mlamins.ngom@gmail.com>
2024-08-09 18:09:16 +0200

    Add new allowed origin

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:8080",
   "https://weconnect-dev.vercel.app",
+  "https://weconnect-k147.onrender.com",
 ];
 
 module.exports = allowedOrigins;
