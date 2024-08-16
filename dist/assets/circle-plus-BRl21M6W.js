4a6293033592f304e58c5413c643e2a41eafa307
mh-lamine <mlamins.ngom@gmail.com>
2024-08-17 01:23:15 +0200

    Remove unused files and update dependencies

---
+++ dist/assets/circle-plus-BRl21M6W.js
@@ -0,0 +1,11 @@
+import{K as c}from"./index-QoA8-fy5.js";/**
+ * @license lucide-react v0.400.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=c("CircleMinus",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M8 12h8",key:"1wcyev"}]]);/**
+ * @license lucide-react v0.400.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=c("CirclePlus",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M8 12h8",key:"1wcyev"}],["path",{d:"M12 8v8",key:"napkw2"}]]);export{a as C,r as a};
