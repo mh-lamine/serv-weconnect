287f17f8865207f2f22d64641994fe54775dfbe7
mh-lamine <mlamins.ngom@gmail.com>
2024-08-18 18:24:26 +0200

    Update script and remove unused files

--- dist/assets/circle-plus-BvditXK0.js
+++ dist/assets/circle-plus-BvditXK0.js
@@ -1,4 +1,4 @@
-import{K as c}from"./index-CP035eP3.js";/**
+import{K as c}from"./index-Bshh90cy.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
