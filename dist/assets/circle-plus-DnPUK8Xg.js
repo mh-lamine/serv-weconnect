6f66561ad452bb808a7091889f6bf3761f34fc5d
mh-lamine <mlamins.ngom@gmail.com>
2024-08-22 17:52:24 +0200

    Update script and stylesheet references

--- dist/assets/circle-plus-DnPUK8Xg.js
+++ dist/assets/circle-plus-DnPUK8Xg.js
@@ -1,4 +1,4 @@
-import{K as c}from"./index-Bshh90cy.js";/**
+import{J as c}from"./index-BD-xntd6.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
