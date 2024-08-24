cbdaa9b31ebf8b0aefb19f85456c2b848dce4ee6
mh-lamine <mlamins.ngom@gmail.com>
2024-08-24 16:17:53 +0200

    Remove unused files and update dependencies

--- dist/assets/circle-plus-CwUCE5MF.js
+++ dist/assets/circle-plus-CwUCE5MF.js
@@ -1,4 +1,4 @@
-import{m as c}from"./index-jl8fpB6g.js";/**
+import{o as c}from"./index-YU60Lnj9.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
