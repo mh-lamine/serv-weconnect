a389f03e61c514471fec43054b5d40304e509bc8
mh-lamine <mlamins.ngom@gmail.com>
2024-09-02 10:49:48 +0200

    Delete unnecessary files and update server configuration

--- src/server.js
+++ src/server.js
@@ -1,5 +1,32 @@
+const express = require("express");
 require("dotenv").config();
-const app = require("./app");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const appointmentRoutes = require("./routes/appointmentRoutes");
+const availabilityRoutes = require("./routes/availabilityRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
+const providerCategoryRoutes = require("./routes/providerCategoryRoutes");
+const providerServiceRoutes = require("./routes/providerServiceRoutes");
+const tagsRoutes = require("./routes/tagsRoutes");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/availabilities", availabilityRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/providerCategory", providerCategoryRoutes);
+app.use("/api/providerService", providerServiceRoutes);
+app.use("/api/tags", tagsRoutes);
 
 const PORT = process.env.PORT || 8080;
 
