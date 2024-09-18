2a9d01bb1aff7298db3b18938bc9b724a21f6918
mh-lamine <mlamins.ngom@gmail.com>
2024-09-18 17:27:58 +0200

    Remove unused files and update dependencies

--- static/pro-weconnect/assets/breadcrumb-DcxVcx4h.js
+++ static/pro-weconnect/assets/breadcrumb-DcxVcx4h.js
@@ -1,4 +1,4 @@
-import{c as d,r as t,j as s,a as o,S as n}from"./index-CXr6LOs3.js";/**
+import{c as d,r as t,j as s,a as o,S as n}from"./index-Dc3UHnjY.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- static/pro-weconnect/assets/circle-plus-BzHbsjoe.js
+++ static/pro-weconnect/assets/circle-plus-BzHbsjoe.js
@@ -1,4 +1,4 @@
-import{c}from"./index-CXr6LOs3.js";/**
+import{c}from"./index-Dc3UHnjY.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
