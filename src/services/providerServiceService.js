5b6c6a6e13dcb2c975475e2a741f3571d990ec78
mh-lamine <mlamins.ngom@gmail.com>
2025-01-31 00:49:34 +0100

    Refactor createProviderService function

--- src/services/providerServiceService.js
+++ src/services/providerServiceService.js
@@ -4,36 +4,13 @@ const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 exports.createProviderService = async (id, role, data) => {
-  const { service, stripeAccountId } = data;
   const ref = role === "PRO" ? "proId" : role === "SALON" ? "salonId" : null;
-  let stripePriceId = null;
-
-  if (stripeAccountId) {
-    try {
-      const { id } = await stripe.prices.create(
-        {
-          currency: "eur",
-          unit_amount: service.price * 100,
-          product_data: {
-            name: service.name,
-          },
-        },
-        { stripeAccount: stripeAccountId }
-      );
-      stripePriceId = id;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  const body = {
-    ...service,
-    [ref]: id,
-    stripePriceId,
-  };
 
   return await prisma.providerService.create({
-    data: { ...body },
+    data: {
+      ...data,
+      [ref]: id,
+    },
   });
 };
 
