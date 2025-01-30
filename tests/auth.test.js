7144025f5bc47820b9f463ebbd6011e450578a36
mh-lamine <mlamins.ngom@gmail.com>
2025-01-31 00:53:50 +0100

    Update dependencies and add Playwright test command

---
+++ tests/auth.test.js
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+
+/**
+ * register a new client user
+ * login the new client user
+ * register a new salon
+ * login the new salon
+ */
+
+const clientUser = {
+  firstName: "User",
+  lastName: "Test",
+  phoneNumber: "0788888888",
+  password: "UserTest1",
+};
+
+test.describe("Register a new client user", () => {
+  test("Should register a new client user", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+    await page.getByTestId("drawerToggle").click({ timeout: 5000 });
+    await page.getByTestId("login").click();
+    await page.getByTestId("register").click();
+    await page.getByTestId("firstName").fill(clientUser.firstName);
+    await page.getByTestId("lastName").fill(clientUser.lastName);
+    await page.getByTestId("phoneNumber").fill(clientUser.phoneNumber);
+    await page.getByTestId("password").fill(clientUser.password);
+    await page.getByTestId("submit").click();
+    await page.waitForURL("http://localhost:5173/");
+  });
+});
